919cc045713be886f63b182afa3ad931a5fc6a40
EmoGeorgiev <unstabl3@Emo>
2025-01-16 19:12:47 +0200

    Added the BookRepository interface

---
+++ Bookreads-backend/src/main/java/com/Bookreads/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.Bookreads.repository;
+
+import com.Bookreads.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
