f670fadc67550ea54f563a59b84268bbf3302b3b
EmoGeorgiev <unstabl3@Emo>
2025-01-23 18:04:56 +0200

    Added initial version of BookMapper

---
+++ Bookreads-backend/src/main/java/com/Bookreads/mapper/BookMapper.java
@@ -0,0 +1,38 @@
+package com.Bookreads.mapper;
+
+import com.Bookreads.dto.BookDto;
+import com.Bookreads.model.Book;
+
+public class BookMapper {
+
+    public BookMapper() {
+
+    }
+    public static BookDto bookToBookDto(Book book) {
+        return new BookDto(
+                book.getId(),
+                book.getTitle(),
+                book.getAuthor(),
+                book.getPageCount(),
+                book.getRating(),
+                book.getReview(),
+                book.getDateRead(),
+                book.getBookshelf(),
+                book.getUser().getId()
+        );
+    }
+
+    public static Book bookDtoToBook(BookDto bookDto) {
+        Book book = new Book();
+        book.setId(bookDto.id());
+        book.setTitle(bookDto.title());
+        book.setAuthor(bookDto.author());
+        book.setPageCount(bookDto.pageCount());
+        book.setRating(bookDto.rating());
+        book.setReview(bookDto.review());
+        book.setDateRead(bookDto.dateRead());
+        book.setBookshelf(bookDto.bookshelf());
+
+        return book;
+    }
+}
