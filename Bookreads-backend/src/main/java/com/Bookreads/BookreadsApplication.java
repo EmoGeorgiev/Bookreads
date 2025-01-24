c1916e777ef8b9710bc47ce4a804484d463be0af
EmoGeorgiev <unstabl3@Emo>
2025-01-24 16:24:14 +0200

    Added Spring security dependency and created Rsa keys

--- Bookreads-backend/src/main/java/com/Bookreads/BookreadsApplication.java
+++ Bookreads-backend/src/main/java/com/Bookreads/BookreadsApplication.java
@@ -1,9 +1,12 @@
 package com.Bookreads;
 
+import com.Bookreads.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class BookreadsApplication {
 
 	public static void main(String[] args) {
