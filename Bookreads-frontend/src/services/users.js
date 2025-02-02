0fc5cce359b56e9ce4cf70c8ea391586a3175922
EmoGeorgiev <unstabl3@Emo>
2025-02-02 19:34:15 +0200

    Added a user service

---
+++ Bookreads-frontend/src/services/users.js
@@ -0,0 +1,30 @@
+import axiosInstance from './axiosInstance'
+
+const baseUrl = '/users'
+
+const getUser = async id => {
+    const response = await axiosInstance.get(`${baseUrl}/${id}`)
+    return response.data
+}
+
+const getUsers = async () => {
+    const response = await axiosInstance.get(baseUrl)
+    return response.data
+}
+
+const updateUser = async (id, user) => {
+    const response = await axiosInstance.put(`${baseUrl}/${id}`, user)
+    return response.data
+}
+
+const updatePassword = async (id, oldPassword, newPassword) => {
+    const response = await axiosInstance.put(`${baseUrl}/${id}/password`, { oldPassword, newPassword })
+    return response.data
+}
+
+const deleteUser = async id => {
+    const response = await axiosInstance.delete(`${baseUrl}/${id}`)
+    return response.data
+}
+
+export default { getUser, getUsers, updateUser, updatePassword, deleteUser }
