033010029d58bb959603734fae97622ab7511636
EmoGeorgiev <unstabl3@Emo>
2025-02-05 23:19:43 +0200

    Added styles to the Home component

--- Bookreads-frontend/src/components/Common/Home.jsx
+++ Bookreads-frontend/src/components/Common/Home.jsx
@@ -1,8 +1,12 @@
+import { useAuth } from '../Auth/AuthContext'
 
 const Home = () => {
+    const { user } = useAuth()
+
     return (
         <div>
-            <h1>Home</h1>
+            <h1 className='m-10 text-5xl text-center font-semibold'>Home</h1>
+            <h3 className='text-4xl text-center font-serif'>Welcome, {user.username}, to Bookreads</h3>
         </div>
     )
 }
