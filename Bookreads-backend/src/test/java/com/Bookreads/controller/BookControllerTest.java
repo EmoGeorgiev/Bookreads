9ff5946eceb5f5ddba1e75fcc007e3aab43abd88
EmoGeorgiev <unstabl3@Emo>
2025-02-28 00:20:10 +0200

    Added tests methods for put and post requests and refactored the code use @BeforeEach and the new error constants

--- Bookreads-backend/src/test/java/com/Bookreads/controller/BookControllerTest.java
+++ Bookreads-backend/src/test/java/com/Bookreads/controller/BookControllerTest.java
@@ -2,136 +2,207 @@ package com.Bookreads.controller;
 
 import com.Bookreads.dto.BookDto;
 import com.Bookreads.enums.Bookshelf;
+import com.Bookreads.exception.BookAlreadyExistsException;
 import com.Bookreads.exception.BookNotFoundException;
 import com.Bookreads.exception.UserNotFoundException;
 import com.Bookreads.service.BookService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.LocalDate;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.anyLong;
+import static com.Bookreads.constants.ErrorMessages.*;
 import static org.mockito.Mockito.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(BookController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class BookControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
-
+    @Autowired
+    private ObjectMapper objectMapper;
     @MockBean
     private BookService bookService;
 
-    @Test
-    @WithMockUser
-    public void shouldReturnNotFoundForNonExistentUserId() throws Exception {
-        when(bookService.getBooksByUserId(anyLong()))
-                .thenThrow(new UserNotFoundException("There does not exist a user with such an id"));
+    private BookDto validBookDto;
+    private BookDto invalidTitleBookDto;
+    private BookDto invalidUserIdBookDto;
 
-        mockMvc.perform(get("/api/books/-1"))
-                .andExpect(status().isNotFound())
-                .andExpect(jsonPath("$.message").value("There does not exist a user with such an id"));;
-
-        verify(bookService).getBooksByUserId(anyLong());
+    @BeforeEach
+    public void init() {
+        validBookDto = new BookDto(1L, "It", "Stephen King", 1000, 5, "", null, Bookshelf.READ,1L);
+        invalidTitleBookDto = new BookDto(1L, "   ", "Stephen King", 1000, 5, "", null, Bookshelf.READ,1L);
+        invalidUserIdBookDto = new BookDto(1L, "It", "Stephen King", 1000, 5, "", null, Bookshelf.READ,-1L);
     }
 
     @Test
-    @WithMockUser
-    public void shouldReturnBooksForGivenUserId() throws Exception {
-        BookDto bookDto = new BookDto(1L, "It", "Stephen King", 1000, 5, "", LocalDate.now(), Bookshelf.READ,1L);
+    public void shouldReturnNotFoundForNonExistingUserIdWhenGettingBooks() throws Exception {
+        when(bookService.getBooksByUserId(-1L))
+                .thenThrow(new UserNotFoundException(USER_NOT_FOUND_MESSAGE));
 
+        mockMvc.perform(get("/api/books/-1"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value(USER_NOT_FOUND_MESSAGE));
+
+        verify(bookService).getBooksByUserId(-1L);
+    }
+
+    @Test
+    public void shouldReturnBooksForGivenUserId() throws Exception {
         when(bookService.getBooksByUserId(1L))
-                .thenReturn(List.of(bookDto));
+                .thenReturn(List.of(validBookDto));
 
         mockMvc.perform(get("/api/books/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].id").value(bookDto.id()))
-                .andExpect(jsonPath("$[0].title").value(bookDto.title()))
-                .andExpect(jsonPath("$[0].author").value(bookDto.author()))
-                .andExpect(jsonPath("$[0].pageCount").value(bookDto.pageCount()))
-                .andExpect(jsonPath("$[0].rating").value(bookDto.rating()))
-                .andExpect(jsonPath("$[0].review").value(bookDto.review()))
-                .andExpect(jsonPath("$[0].dateRead").value(bookDto.dateRead().toString()))
-                .andExpect(jsonPath("$[0].bookshelf").value(bookDto.bookshelf().name()))
-                .andExpect(jsonPath("$[0].userId").value(bookDto.userId()));
+                .andExpect(jsonPath("$[0].id").value(validBookDto.id()))
+                .andExpect(jsonPath("$[0].title").value(validBookDto.title()))
+                .andExpect(jsonPath("$[0].author").value(validBookDto.author()))
+                .andExpect(jsonPath("$[0].pageCount").value(validBookDto.pageCount()))
+                .andExpect(jsonPath("$[0].rating").value(validBookDto.rating()))
+                .andExpect(jsonPath("$[0].review").value(validBookDto.review()))
+                .andExpect(jsonPath("$[0].userId").value(validBookDto.userId()));
 
         verify(bookService).getBooksByUserId(1L);
     }
 
     @Test
-    @WithMockUser
     public void shouldCreateABookAndReturnTheBookResult() throws Exception {
-        BookDto bookDto = new BookDto(1L, "It", "Stephen King", 1000, 5, "", null, Bookshelf.READ,1L);
-
-        when(bookService.addBook(bookDto))
-                .thenReturn(bookDto);
+        when(bookService.addBook(validBookDto))
+                .thenReturn(validBookDto);
 
         mockMvc.perform(post("/api/books")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(bookDto))
-                        .with(csrf()))
+                        .content(objectMapper.writeValueAsString(validBookDto)))
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.id").value(bookDto.id()))
-                .andExpect(jsonPath("$.title").value(bookDto.title()))
-                .andExpect(jsonPath("$.author").value(bookDto.author()))
-                .andExpect(jsonPath("$.pageCount").value(bookDto.pageCount()))
-                .andExpect(jsonPath("$.rating").value(bookDto.rating()))
-                .andExpect(jsonPath("$.review").value(bookDto.review()))
-                .andExpect(jsonPath("$.bookshelf").value(bookDto.bookshelf().name()))
-                .andExpect(jsonPath("$.userId").value(bookDto.userId()));
+                .andExpect(jsonPath("$.id").value(validBookDto.id()))
+                .andExpect(jsonPath("$.title").value(validBookDto.title()))
+                .andExpect(jsonPath("$.author").value(validBookDto.author()))
+                .andExpect(jsonPath("$.pageCount").value(validBookDto.pageCount()))
+                .andExpect(jsonPath("$.rating").value(validBookDto.rating()))
+                .andExpect(jsonPath("$.review").value(validBookDto.review()))
+                .andExpect(jsonPath("$.bookshelf").value(validBookDto.bookshelf().name()))
+                .andExpect(jsonPath("$.userId").value(validBookDto.userId()));
 
-        verify(bookService).addBook(bookDto);
+        verify(bookService).addBook(validBookDto);
     }
 
     @Test
-    @WithMockUser
-    public void shouldReturnBadRequestWhenFieldIsInvalid() throws Exception {
-        BookDto bookDto = new BookDto(1L, "", "Stephen King", 1000, 5, "", null, Bookshelf.READ,1L);
+    public void shouldReturnBadRequestWhenTitleIsInvalid() throws Exception {
+        mockMvc.perform(post("/api/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(invalidTitleBookDto)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.title").value("Title cannot be blank"));
+    }
+
+    @Test
+    public void shouldReturnNotFoundForNonExistingUserIdWhenAddingBook() throws Exception {
+        when(bookService.addBook(invalidUserIdBookDto))
+                .thenThrow(new UserNotFoundException(USER_NOT_FOUND_MESSAGE));
 
         mockMvc.perform(post("/api/books")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(bookDto))
-                        .with(csrf()))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.title").value("Title must be between 1 and 100 characters"));
+                        .content(objectMapper.writeValueAsString(invalidUserIdBookDto)))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value(USER_NOT_FOUND_MESSAGE));
+
+        verify(bookService).addBook(invalidUserIdBookDto);
     }
 
     @Test
-    @WithMockUser
-    public void shouldDeleteBookWhenBookIdIsValidAndReturnNoContent() throws Exception {
-        doNothing().when(bookService).deleteBook(anyLong());
+    public void shouldReturnConflictForDuplicateBook() throws Exception {
+        when(bookService.addBook(validBookDto))
+                .thenThrow(new BookAlreadyExistsException(BOOK_ALREADY_EXISTS_MESSAGE));
 
-        mockMvc.perform(delete("/api/books/1")
-                        .with(csrf()))
+        mockMvc.perform(post("/api/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(validBookDto)))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.message").value(BOOK_ALREADY_EXISTS_MESSAGE));
+
+        verify(bookService).addBook(validBookDto);
+    }
+
+    @Test
+    public void shouldUpdateBookWhenBookIdIsValidAndReturnTheBookResult() throws Exception {
+        when(bookService.updateBook(1L, validBookDto))
+                .thenReturn(validBookDto);
+
+        mockMvc.perform(put("/api/books/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(validBookDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(validBookDto.id()))
+                .andExpect(jsonPath("$.title").value(validBookDto.title()))
+                .andExpect(jsonPath("$.author").value(validBookDto.author()))
+                .andExpect(jsonPath("$.pageCount").value(validBookDto.pageCount()))
+                .andExpect(jsonPath("$.rating").value(validBookDto.rating()))
+                .andExpect(jsonPath("$.review").value(validBookDto.review()))
+                .andExpect(jsonPath("$.bookshelf").value(validBookDto.bookshelf().name()))
+                .andExpect(jsonPath("$.userId").value(validBookDto.userId()));
+
+        verify(bookService).updateBook(1L, validBookDto);
+    }
+
+    @Test
+    public void shouldReturnNotFoundForNonExistingBookIdWhenUpdatingBook() throws Exception {
+        when(bookService.updateBook(-1L, validBookDto))
+                .thenThrow(new BookNotFoundException(BOOK_NOT_FOUND_MESSAGE));
+
+        mockMvc.perform(put("/api/books/{id}", -1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(validBookDto)))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value(BOOK_NOT_FOUND_MESSAGE));;
+
+        verify(bookService).updateBook(-1L, validBookDto);
+    }
+
+    @Test
+    public void shouldReturnNotFoundForNonExistingUserIdWhenUpdatingBook() throws Exception {
+        when(bookService.updateBook(1L, invalidUserIdBookDto))
+                .thenThrow(new UserNotFoundException(USER_NOT_FOUND_MESSAGE));
+
+        mockMvc.perform(put("/api/books/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(invalidUserIdBookDto)))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value(USER_NOT_FOUND_MESSAGE));;
+
+        verify(bookService).updateBook(1L, invalidUserIdBookDto);
+    }
+
+    @Test
+    public void shouldDeleteBookWhenBookIdIsValidAndReturnNoContent() throws Exception {
+        doNothing().when(bookService).deleteBook(1L);
+
+        mockMvc.perform(delete("/api/books/1"))
                 .andExpect(status().isNoContent());
 
-        verify(bookService).deleteBook(anyLong());
+        verify(bookService).deleteBook(1L);
     }
 
     @Test
-    @WithMockUser
-    public void shouldReturnNotFoundForNonExistingBookId() throws Exception {
-        doThrow(new BookNotFoundException("There does not exist a book with such an id"))
-                .when(bookService).deleteBook(anyLong());
+    public void shouldReturnNotFoundForNonExistingBookIdWhenDeletingBook() throws Exception {
+        doThrow(new BookNotFoundException(BOOK_NOT_FOUND_MESSAGE))
+                .when(bookService).deleteBook(-1L);
 
-        mockMvc.perform(delete("/api/books/1")
-                        .with(csrf()))
+        mockMvc.perform(delete("/api/books/-1"))
                 .andExpect(status().isNotFound())
-                .andExpect(jsonPath("$.message").value("There does not exist a book with such an id"));
+                .andExpect(jsonPath("$.message").value(BOOK_NOT_FOUND_MESSAGE));
 
-        verify(bookService).deleteBook(anyLong());
+        verify(bookService).deleteBook(-1L);
     }
 }
