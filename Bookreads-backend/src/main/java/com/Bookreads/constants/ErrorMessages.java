cadf62fe9710bf6eec865dddc1c9e4c6b30380cc
EmoGeorgiev <unstabl3@Emo>
2025-02-28 00:17:52 +0200

    Added constants for error messages

---
+++ Bookreads-backend/src/main/java/com/Bookreads/constants/ErrorMessages.java
@@ -0,0 +1,13 @@
+package com.Bookreads.constants;
+
+public class ErrorMessages {
+    public static final String BOOK_NOT_FOUND_MESSAGE = "There does not exist a book with such an id";
+    public static final String BOOK_ALREADY_EXISTS_MESSAGE = "This book has already been added";
+     public static final String PAGE_COUNT_CANNOT_BE_LESS_THAN_ONE = "The passwords do not match";
+    public static final String PASSWORDS_DO_NOT_MATCH = "The passwords do not match";
+    public static final String RATING_MUST_BE_BETWEEN_ONE_AND_FIVE = "The passwords do not match";
+    public static final String USER_NOT_FOUND_MESSAGE = "There does not exist a user with such an id";
+    public static final String USERNAME_DOES_NOT_EXIST = "There does not exist a user with this username";
+    public static final String USERNAME_ALREADY_EXISTS = "This username already exists";
+
+}
