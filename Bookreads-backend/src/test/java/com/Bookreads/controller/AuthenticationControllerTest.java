cc035eda53226da2c97977b705b5fdd162fc5de3
EmoGeorgiev <unstabl3@Emo>
2025-03-04 20:10:55 +0200

    Added tests for the AuthenticationController class

---
+++ Bookreads-backend/src/test/java/com/Bookreads/controller/AuthenticationControllerTest.java
@@ -0,0 +1,58 @@
+package com.Bookreads.controller;
+
+import com.Bookreads.dto.LoginResponseDto;
+import com.Bookreads.service.AuthenticationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AuthenticationController.class)
+public class AuthenticationControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private AuthenticationService authenticationService;
+
+    @Test
+    public void shouldReturnIsUnauthorizedForWrongCredentials() throws Exception {
+        mockMvc.perform(post("/api/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{}")
+                        .with(csrf()))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnLoginResponseDtoForValidCredentials() throws Exception {
+        String token = "testToken";
+        LoginResponseDto loginResponseDto = new LoginResponseDto("username", 1L, token);
+
+        when(authenticationService.login(any(Authentication.class)))
+                .thenReturn(loginResponseDto);
+
+        mockMvc.perform(post("/api/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\\\"username\\\": \\\"username\\\", \\\"password\\\": \\\"password\\\"}")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value(loginResponseDto.username()))
+                .andExpect(jsonPath("$.id").value(loginResponseDto.id()))
+                .andExpect(jsonPath("$.token").value(token));
+
+        verify(authenticationService).login(any(Authentication.class));
+    }
+}
